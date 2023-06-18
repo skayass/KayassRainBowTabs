b1a8f6cf4403d4cba03a0fbc8dafedc3646a80c8
SAMIA KAYASS <119642825+skayass@users.noreply.github.com>
2023-06-18 16:21:23 -0500

    Add files via upload

---
+++ KayassRainbowTabs/RedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  KayassRainbowTabs
+//
+//  Created by test on 10/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBarItem.badgeValue = nil
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
